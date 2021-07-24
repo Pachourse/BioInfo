dfd86370446a78600bd7f63f9e2fdc442fc578b1
Paviel Schertzer <paviel.schertzer@epita.fr>
2021-07-24 13:49:35 +0200

    ARG PARSER : updater to better use in futur

--- src/bw-build.py
+++ src/bw-build.py
@@ -2,24 +2,41 @@ import sys
 
 debug = False
 
-def main(infile, outfile, f, compression) :
+#global params
+compression = False
+
+def main(infile, outfile, f) :
     if debug == True :
-        print(infile, outfile, f, compression)
-    return 1
+        print("compression : ", compression)
+        print(infile, outfile, f)
+    return 0
 
 
 if __name__ == "__main__" :
+    compression = False
     argv = sys.argv
-    print(len(argv))
-    if len(argv) == 2 and argv[1] == "--h" :
-        print("this is help menu")
-    elif len(argv) == 5 :
-        if argv[4] == "--compression" :
-            print("A")
-            main(infile=argv[1], outfile=argv[2], f=argv[3], compression=True)
+
+    options = []
+    input = []
+
+    #argc/argv parser
+    for arg in argv :
+        if "bw-build.py" in arg :
+            continue
+        elif "--" in arg :
+            options.append(arg)
         else :
-            main(infile=argv[1], outfile=argv[2], f=argv[3], compression=False)
-    elif len(argv) == 4 :
-        main(infile=argv[1], outfile=argv[2], f=argv[3], compression=False)
-    else : 
-        print("Please : add argv arguments")
+            input.append(arg)
+
+    # global options params
+    if "--compression" in options :
+        compression = True
+    
+    if (len(input)) < 3 :
+        raise Exception("ERROR : default case, please add arguments : \n \t infile, outflie and f argument")
+
+    if debug == True :
+        print("options : ", options)
+        print('input : ', input)
+
+    main(infile=input[0], outfile=input[1], f=input[2])
